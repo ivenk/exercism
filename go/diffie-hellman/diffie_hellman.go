f9457a24dbb693a4df7c6e44722d6a2f2712a026
iven_k <koethke@th-brandenburg.de>
2019-01-03 17:39:44 +0100

    Started working on diffie hellmann exercise

---
+++ go/diffie-hellman/diffie_hellman.go
@@ -0,0 +1,34 @@
+package diffiehellman
+
+import (
+	"crypto/rand"
+	"io"
+	"math/big"
+)
+
+func PrivateKey(prime *big.Int) *big.Int {
+	var Reader io.Reader
+
+	prime.Add(prime, big.NewInt(-2)) // we subtract two from the prime number to make range go from 0 to prime -2, later we add one to change range to 1 to prime -1
+
+	key, err := rand.Int(Reader, prime)
+	// read up on check error handling in go
+	if err != nil {
+		panic("Private key creation failed !")
+	}
+	// we add one to shift range to correct area
+	key.Add(key, big.NewInt(1))
+	return key
+}
+
+func PublicKey(privateKey *big.Int, p *big.Int, g *big.Int) *big.Int {
+	mod := new(big.Int)
+	return mod.Mod((g.Exp(g, privateKey, nil)), p)
+}
+
+func NewPair(p *big.Int, g int64) (private, public *big.Int) {
+
+}
+
+func SecretKey(private, public, p *big.Int) *big.Int {
+}
