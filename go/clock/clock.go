d58ef166fba858761819fe1eb57a0ea0c3e4c23f
iven_k <koethke@th-brandenburg.de>
2019-01-06 18:53:43 +0100

    Removed unnecessary checkbounds method and changed the clock struct to a custom primitiv.

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -3,50 +3,40 @@ package clock
 import "fmt"
 
 // Clock respresents a real world clock, but misses seconds
-type Clock struct {
-	minute int
-}
+type Clock int
 
 // New initialises a new clock with the given time
 func New(hour int, minute int) Clock {
-	var min = hour * 60 // may be negativ
-	min += minute       // again may be negativ
-	clockHelper := Clock{min}
-	return clockHelper.checkBounds()
+	var min = (hour * 60) + minute // may be negativ
+	minPerDay := 24 * 60
+
+	if min < 0 {
+		var days int
+		days = (-1 * (min / minPerDay)) + 1
+		min += minPerDay * days
+	}
+
+	for min >= minPerDay {
+		min -= minPerDay
+	}
+
+	return Clock(min)
 }
 
 // Add adds the given amount of minutes to the clock
 func (clock Clock) Add(minute int) Clock {
-	clock.minute += minute
-	return clock.checkBounds()
+	return New(0, int(clock)+minute)
 }
 
 // Subtract subtracts the given amount of minutes from the clock
 func (clock Clock) Subtract(minute int) Clock {
 	// invert minutes
 	minute = minute * -1
-	return clock.Add(minute)
+	return New(0, int(clock)+minute)
 }
 
 func (clock Clock) String() string {
-	hour := clock.minute / 60
-	min := clock.minute % 60
+	hour := int(clock) / 60
+	min := int(clock) % 60
 	return fmt.Sprintf("%02d:%02d", hour, min)
-}
-
-// checkBounds enforces the time format
-func (clock Clock) checkBounds() Clock {
-	minPerDay := 24 * 60
-
-	if clock.minute < 0 {
-		var days int
-		days = (-1 * (clock.minute / minPerDay)) + 1
-		clock.minute += minPerDay * days
-	}
-
-	for clock.minute >= minPerDay {
-		clock.minute -= minPerDay
-	}
-
-	return clock
 }
