7795be2577899b283d50b2128ac154a4db61c4ab
iven_k <koethke@th-brandenburg.de>
2019-01-06 03:39:24 +0100

    New improved version for the clock exercise ! The clock only stores minutes now. Improved the strin output and general readability.

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -4,13 +4,14 @@ import "fmt"
 
 // Clock respresents a real world clock, but misses seconds
 type Clock struct {
-	hour   int
 	minute int
 }
 
 // New initialises a new clock with the given time
 func New(hour int, minute int) Clock {
-	clockHelper := Clock{hour, minute}
+	var min = hour * 60 // may be negativ
+	min += minute       // again may be negativ
+	clockHelper := Clock{min}
 	return clockHelper.checkBounds()
 }
 
@@ -28,65 +29,23 @@ func (clock Clock) Subtract(minute int) Clock {
 }
 
 func (clock Clock) String() string {
-	// in order for add to perform hours and minutes limit checks
-	nc := clock.Add(0)
-	hour := nc.hour
-	minute := nc.minute
-	var hs string
-	var ms string
-	if hour < 10 && hour >= 0 {
-		hs = fmt.Sprintf("0%d", hour)
-	} else {
-		hs = fmt.Sprintf("%d", hour)
-	}
-
-	if minute < 10 && minute >= 0 {
-		ms = fmt.Sprintf("0%d", minute)
-	} else {
-		ms = fmt.Sprintf("%d", minute)
-	}
-
-	return fmt.Sprintf("%s:%s", hs, ms)
+	hour := clock.minute / 60
+	min := clock.minute % 60
+	return fmt.Sprintf("%02d:%02d", hour, min)
 }
 
+// checkBounds enforces the time format
 func (clock Clock) checkBounds() Clock {
-	// are we over 59 minutes
-	if clock.minute > 59 {
-		// next hour
-		clock.hour = clock.hour + int(clock.minute/60)
-		clock.minute = clock.minute % 60
-	}
-	// are we under 0 minutes
+	minPerDay := 24 * 60
+
 	if clock.minute < 0 {
-		var sub = int(clock.minute / 60)
-		if sub < 0 {
-			sub = sub * -1
-		}
-		clock.hour = clock.hour - sub
-		if (clock.minute % 60) != 0 {
-			clock.hour = clock.hour - 1
-		} else {
-			clock.minute = clock.minute % 60
-
-		}
-		clock.minute = clock.minute % 60
-		if clock.minute != 0 {
-			clock.minute += 60
-		}
+		var days int
+		days = (-1 * (clock.minute / minPerDay)) + 1
+		clock.minute += minPerDay * days
 	}
 
-	// are we over 23 hours
-	if clock.hour > 23 {
-		clock.hour = clock.hour % 24
-	}
-
-	// are we under 0 hours
-	if clock.hour < 0 {
-		for clock.hour < 0 {
-			clock.hour += 24
-		}
-
-		clock.hour = clock.hour % 24
+	for clock.minute >= minPerDay {
+		clock.minute -= minPerDay
 	}
 
 	return clock
