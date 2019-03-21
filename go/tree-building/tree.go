8d4772917c7e1b1ee5b9a33b28f4ffb03bf6e18b
iven_k <koethke@th-brandenburg.de>
2019-03-21 03:43:26 +0100

    updated some stuff, still not working...

--- go/tree-building/tree.go
+++ go/tree-building/tree.go
@@ -26,40 +26,38 @@ func Build(records []Record) (*Node, error) {
 
 	for i, x := range slice {
 		fmt.Printf("id: %d\nparent: %d\n", x.ID, x.Parent)
-		if (slice[i].ID != slice[i].ID) || (slice[i].ID != slice[i].ID+1) {
-			return nil, errors.New("fail")
+		if len(slice) > 1 {
+			if (slice[i].ID != slice[i].ID) || (slice[i].ID != slice[i].ID+1) {
+				return nil, errors.New("fail2")
+			}
 		}
 	}
 
 	if len(slice) > 0 {
 		// root should have itself as a parent
 		if slice[0].Parent != slice[0].ID {
-			return nil, errors.New("fail")
+			return nil, errors.New("fail1")
 		}
 	}
 
 	// create *Node
 	var node *Node
 	n := make([]*Node, 0)
+	fmt.Println(n)
 
 	for len(slice) > 0 {
 		r := slice[0]
 		slice = slice[1:] // remove first element
 
 		for _, v := range slice {
-			// if we have duplicate ids
-			if v.ID == r.ID {
-				return nil, errors.New("fail")
-			}
-
 			// look for records with matching parent id; exlude root parent != ownID
 			if v.Parent == r.ID && v.Parent != v.ID {
 				n = append(n, &Node{v.Parent, []*Node{}})
 			}
-
 		}
 		node = &Node{r.ID, n}
 	}
 
+	fmt.Print(node)
 	return node, nil
 }
