de286f8b57f0a5af9ca29faa7916dfcb423530a0
iven_k <koethke@th-brandenburg.de>
2019-04-03 02:05:20 +0200

    Updated a lot functionality. Some tests pass now.

--- go/tree-building/tree.go
+++ go/tree-building/tree.go
@@ -1,8 +1,9 @@
 package tree
 
 import (
-//	"errors"
-//	"fmt"
+	"errors"
+	"fmt"
+	"sort"
 )
 
 type Record struct {
@@ -15,17 +16,43 @@ type Node struct {
 	Children []*Node
 }
 
+// special type sorting
+type ByID []Record
+
+func (rec ByID) Len() int {
+	return len(rec)
+}
+
+func (rec ByID) Swap(i, j int) {
+	rec[i], rec[j] = rec[j], rec[i]
+}
+
+func (rec ByID) Less(i, j int) bool {
+	return rec[i].ID < rec[j].ID
+}
+
 func Build(records []Record) (*Node, error) {
+	// error checks
 	if len(records) == 0 {
 		return nil, nil
 	}
 	if len(records) == 1 {
 		return &Node{0, nil}, nil
 	}
+	workingParent := false
+	for _, x := range records {
+		fmt.Println("Records:")
+		fmt.Print("      ")
+		fmt.Println(x)
+		if (x.ID == x.Parent) && (x.ID == 0) { // node with id 0 and id = parentID
+			workingParent = true
+		}
+	}
+	if !workingParent {
+		return nil, errors.New("Fail !")
+	}
+
 	slice := records[:]
-
-	// need to test for failures
-
 	return buildNode(0, slice), nil
 }
 
@@ -36,26 +63,34 @@ func buildNode(id int, slice []Record) *Node {
 	var childRecords []Record
 	var children []*Node
 
-	var ownIndex int
+	var cutSlice []Record
+	ownIndex := -1
 	// assemble children
 	for i, x := range slice {
 		if x.Parent == id {
-			cutSlice := remove(slice, i)
-			childRecords = append(childRecords, x)
+			if x.ID != id { // only add if not self
+				childRecords = append(childRecords, x)
+			}
 		}
 		// find own position
 		if x.ID == id {
 			ownIndex = i
 		}
 	}
-	cutSlice = remove(cutSlice, ownIndex)
+	// only remove ourself if we found ourself
+	if ownIndex > -1 {
+		cutSlice = remove(slice, ownIndex)
+	}
+
+	// sorts the records by there id
+	sort.Sort(ByID(childRecords))
 
 	//  build rest ...
-	for _, record : = childRecords {
+	for _, record := range childRecords {
 		child := buildNode(record.ID, cutSlice)
 		children = append(children, child)
 	}
-	
+
 	return &Node{id, children}
 }
 
