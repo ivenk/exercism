ecbf669994820483a0fc83e4a323cc4def1da78e
iven_k <koethke@th-brandenburg.de>
2019-04-21 03:12:21 +0200

    Updated comments

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,6 +1,6 @@
-#tried to make my code look more like python
 class Matrix(object):
     def __init__(self, matrix_string):
+        # the matrix represented by a list of rows which are represented by a list aswell
         self.matrix_list = []
         for x in matrix_string.split("\n"):
             self.matrix_list.append([int(y) for y in x.split(" ")])
