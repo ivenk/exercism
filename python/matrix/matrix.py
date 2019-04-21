e085f1a39d8e08f79f80cea2f0edeec9256d7884
iven_k <koethke@th-brandenburg.de>
2019-04-21 03:03:14 +0200

    New version after feedback. Column still missing. Logic now handled in init with a list of list for the matrix

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,11 +1,13 @@
 #tried to make my code look more like python
 class Matrix(object):
     def __init__(self, matrix_string):
-        self.matrix = matrix_string
-
+        self.matrix_list = []
+        for x in matrix_string.split("\n"):
+            self.matrix_list.append([int(y) for y in x.split(" ")])
+        
     def row(self, index):
-        return [int(i) for i in self.matrix.split("\n")[index-1].split(" ")]
+        return self.matrix_list[index-1]
         
     def column(self, index):
-        return [int(i.split(" ")[index-1]) for i in self.matrix.split("\n")]
+        return
         
