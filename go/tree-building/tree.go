aaec66f86623fa17ec850feee73d5c5b66723a01
iven_k <koethke@th-brandenburg.de>
2019-04-24 16:47:26 +0200

    Looked at the amazing solution of luisrpp.

--- go/tree-building/tree.go
+++ go/tree-building/tree.go
@@ -1,3 +1,6 @@
+// mostly not my stuff
+// looked at the amazing solution of luisrpp here on exercism.
+
 package tree
 
 import (
@@ -16,136 +19,36 @@ type Node struct {
 }
 
 func Build(records []Record) (*Node, error) {
-	// error checks
 	if len(records) == 0 {
 		return nil, nil
 	}
 
-	// test if all child nodes have higher ids then parents
-	for _, y := range records {
-		if y.ID < y.Parent {
-			return nil, errors.New("fail")
+	sort.Slice(records, func(i, j int) bool { return records[i].ID < records[j].ID })
+
+	// create a slice of nodes
+	nodes := make([]Node, len(records))
+
+	for i, x := range records {
+		// error checking
+		if i != x.ID {
+			return nil, errors.New("ids not continued")
+		}
+		if x.ID != 0 && x.ID <= x.Parent {
+			return nil, errors.New("id has to be bigger then id of parent")
+		}
+		if x.ID == 0 && x.Parent != 0 {
+			return nil, errors.New("root cannot have a parent")
+		}
+		// --------------
+
+		nodes[x.ID] = Node{ID: x.ID}
+
+		if i != 0 {
+			// get reference from parent, Parent will already be created due to sorting and the lower indexed parents
+			parent := &nodes[x.Parent]
+			parent.Children = append(parent.Children, &nodes[i]) // append the node
 		}
 	}
 
-	lastElements := 0
-	// check for duplicates and non continuous
-	for _, i := range records {
-		occurence := 0
-		hasSuccessor := false
-
-		for _, j := range records {
-			if i.ID == j.ID {
-				occurence += 1
-			}
-			if i.ID == (j.ID + 1) {
-				hasSuccessor = true
-			}
-		}
-
-		if occurence > 1 {
-			return nil, errors.New("fail")
-		}
-
-		// if it does not have a direct successor it must be the last element
-		if !hasSuccessor {
-			lastElements += 1
-		}
-	}
-
-	// if there are more then one elments without successors we have non continiuous items
-	if lastElements > 1 {
-		return nil, errors.New("fail")
-	}
-
-	workingParent := false
-	for _, x := range records {
-		if (x.ID == x.Parent) && (x.ID == 0) { // node with id 0 and id = parentID
-			workingParent = true
-		}
-		if (x.ID == x.Parent) && (x.ID != 0) {
-			return nil, errors.New("fail")
-		}
-	}
-	if !workingParent {
-		return nil, errors.New("Fail !")
-	}
-
-	slice := records[:]
-	tree := buildNode(0, slice)
-
-	// count nodes created; finds cycling
-	count := countTotalChildNodes(tree) + 1
-	if count < len(records) {
-		return nil, errors.New("fail")
-	}
-
-	return tree, nil
-
-}
-
-// builds a node with the given id
-// browses the slice for children nods
-func buildNode(id int, slice []Record) *Node {
-	var childIDs []int
-	var children []*Node
-
-	var cutSlice []Record
-	ownIndex := -1
-	// assemble children
-	for i, x := range slice {
-		if x.Parent == id {
-			if x.ID != id { // only add if not self
-				if !contains(childIDs, x.ID) { // this check should not be requiered...
-					childIDs = append(childIDs, x.ID)
-				}
-			}
-		}
-		// find own position
-		if x.ID == id {
-			ownIndex = i
-		}
-	}
-	// only remove ourself if we found ourself
-	if ownIndex > -1 {
-		cutSlice = remove(slice, ownIndex)
-	}
-
-	// sorts the records by there id
-	sort.Ints(childIDs)
-
-	//  build rest ...
-	for _, record := range childIDs {
-		child := buildNode(record, cutSlice)
-		children = append(children, child)
-	}
-
-	return &Node{id, children}
-}
-
-// Removes element of slice
-// taken from ...
-func remove(slice []Record, index int) []Record {
-	slice[index] = slice[len(slice)-1]
-	return slice[:len(slice)-1]
-}
-
-// checks wether the int value is found within the given slice
-func contains(slice []int, value int) bool {
-	for _, x := range slice {
-		if x == value {
-			return true
-		}
-	}
-	return false
-}
-
-// this counts all the nodes below the current one. Returns the total number of nodes - 1
-func countTotalChildNodes(tree *Node) int {
-	count := len(tree.Children)
-	for _, x := range tree.Children {
-		count += countTotalChildNodes(x)
-	}
-
-	return count
+	return &nodes[0], nil
 }
