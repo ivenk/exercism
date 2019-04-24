35134c1feff43f9db636bdcce8756fb79d59062c
iven_k <koethke@th-brandenburg.de>
2019-04-24 12:48:32 +0200

    After receiving feedback implemented a more pythonic version.

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,12 +1,6 @@
 def distance(strand_a, strand_b):
-    distance = 0
-
     if len(strand_a) != len(strand_b):
         raise ValueError("ValueError")
+    
+    return len([x for x, y in zip(strand_a, strand_b) if x != y])
 
-    # enumerate returns the index and the value
-    for i, x in enumerate(strand_a):
-        if x != strand_b[i]:
-            distance += 1
-
-    return distance
