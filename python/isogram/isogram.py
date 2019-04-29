0acdd9e51302e690900d51cb867933d0bcca18b8
iven_k <koethke@th-brandenburg.de>
2019-04-30 01:25:13 +0200

    Removed 2. solution and imports. Now lowers the enitre string instead of each rune

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,9 +1,10 @@
 def is_isogram(string):
     li = []
-    for x in [c for c in string if c.isalpha()]:
-        x = x.lower()
+    for x in [c for c in string.lower() if c.isalpha()]:
         if x in li:
             return False
         li.append(x)
 
     return True
+
+    
