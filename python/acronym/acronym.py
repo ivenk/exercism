f55a28b55aaf488c2a112034103020c3f3c2ac1e
iven_k <koethke@th-brandenburg.de>
2019-05-08 21:22:05 +0200

    removed old code.

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -3,5 +3,4 @@ import re
 
 def abbreviate(words):
     return ''.join([w.strip('_')[0].upper() for w in re.findall(r"[\w']+", words)])
-    # return ''.join([w.strip('_')[0].upper() for w in words.replace(' -', '').split()])
     
