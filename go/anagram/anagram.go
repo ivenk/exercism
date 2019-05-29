3fbaf5effd1e534b401148b17bb9637687843ce5
iven_k <koethke@th-brandenburg.de>
2019-05-29 17:27:21 +0200

    First version

---
+++ go/anagram/anagram.go
@@ -0,0 +1,35 @@
+package anagram
+
+import (
+	"reflect"
+	"strings"
+)
+
+// Detect returns a subset of strings from the given possibilities which are actually anagrams of the given base
+func Detect(base string, poss []string) []string {
+	result := make([]string, 0)
+	lbase := strings.ToLower(base)
+
+	bm := buildMapFromWord(lbase)
+	for _, p := range poss {
+		lp := strings.ToLower(p)
+		if lp == lbase { // the word itself is no anagram
+			continue
+		}
+		pm := buildMapFromWord(lp)
+		if reflect.DeepEqual(bm, pm) {
+			result = append(result, p)
+		}
+	}
+	return result
+}
+
+// buildMapFromWord returns a map matching each rune to the number of occurences in the given word.
+func buildMapFromWord(word string) map[rune]int {
+	m := make(map[rune]int)
+
+	for _, l := range word {
+		m[l] += 1
+	}
+	return m
+}
