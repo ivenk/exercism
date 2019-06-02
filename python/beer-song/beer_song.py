e8eb6fbbd911d296a96cc3306942ca2222419fc3
iven_k <koethke@th-brandenburg.de>
2019-06-02 15:15:49 +0200

    This looks horrible. Only partially working

--- python/beer-song/beer_song.py
+++ python/beer-song/beer_song.py
@@ -3,5 +3,5 @@ from itertools import chain
 gen_first = "{} bottles of beer on the wall, {} bottles of beer.#Take one down and pass it around, {} bottles of beer on the wall."
 
 def recite(start, take=1):
-    return list(chain.from_iterable([z for z in [y.split('#') for y in [gen_first.format(x, x, x-1) for x in range(start, start-take, -1)]]]))
+    return list(chain.from_iterable([z.split('#') for z in list(chain.from_iterable([y for y in zip([gen_first.format(x, x, x-1) for x in range(start, start-take, -1)], ['']*take)]))]))[:-1]
 
