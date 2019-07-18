0d0b18b59f02a4f77a3f85f667c56d3f2c9d7c7d
Iven <ivenkoethke@hotmail.de>
2019-07-18 20:34:34 +0200

    Completed input parsing. Now only creation of output is left.

--- go/tournament/tournament.go
+++ go/tournament/tournament.go
@@ -3,9 +3,12 @@ package tournament
 import (
 	"errors"
 	"io"
-	"strconv"
 	"strings"
 )
+
+const BufferSize = 1000
+
+const TableHeader = "Team                           | MP |  W |  D |  L |  P"
 
 // input looks like :
 // ---
@@ -15,11 +18,23 @@ import (
 // gets converted to:
 // internal map structure: [name_of_the_team] = [wins, losses]
 // Tally converts the given input string into a table and returns it
-func Tally(io.Reader, io.Writer) error {
-	var results string
+func Tally(in io.Reader, out io.Writer) error {
 	m := make(map[string][2]int)
 
-	for _, l := range strings.Split(results, "\n") { // split on new lines
+	buffer := make([]byte, BufferSize)
+	n, e := in.Read(buffer)
+	if e != nil {
+		return errors.New("Could not read input")
+	}
+	entries := string(buffer[:n])
+
+	// iterate over all lines
+	for _, l := range strings.Split(entries, "\n") {
+		// there are some empty lines in the input
+		if l == "" {
+			continue
+		}
+
 		w := strings.Split(l, ";")
 		if len(w) != 3 {
 			return errors.New("Input data is formated incorrectly")
@@ -37,15 +52,16 @@ func Tally(io.Reader, io.Writer) error {
 		case "win":
 			m[w[0]] = [2]int{m[w[0]][0] + 1, m[w[0]][1]} // add one to wins for team 1
 			m[w[1]] = [2]int{m[w[1]][0], m[w[1]][1] + 1} // second team gets a loss
-		case "draw": // do nothing
-			break
 		case "loss":
 			m[w[0]] = [2]int{m[w[0]][0], m[w[0]][1] + 1} // add one to loss for team 1
 			m[w[1]] = [2]int{m[w[1]][0] + 1, m[w[1]][1]} // second team gets a win
+		case "draw": // do nothing
+			break
 		default:
 			return errors.New("game result could not be parsed for " + w[0] + " : " + w[1])
 		}
 	}
 
-	return ""
+	// build output
+	return nil
 }
