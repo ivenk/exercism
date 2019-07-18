18bc7f5ef3e721271504436dd648daedd7e654dc
Iven <ivenkoethke@hotmail.de>
2019-07-18 22:27:13 +0200

    Added logic for parsing output format. Introduced new struct to hold information. not all tests pass

--- go/tournament/tournament.go
+++ go/tournament/tournament.go
@@ -2,6 +2,7 @@ package tournament
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"strings"
 )
@@ -9,6 +10,13 @@ import (
 const BufferSize = 1000
 
 const TableHeader = "Team                           | MP |  W |  D |  L |  P"
+
+type entry struct {
+	name   string
+	wins   int
+	losses int
+	draws  int
+}
 
 // input looks like :
 // ---
@@ -19,7 +27,7 @@ const TableHeader = "Team                           | MP |  W |  D |  L |  P"
 // internal map structure: [name_of_the_team] = [wins, losses]
 // Tally converts the given input string into a table and returns it
 func Tally(in io.Reader, out io.Writer) error {
-	m := make(map[string][2]int)
+	m := make(map[string]entry)
 
 	buffer := make([]byte, BufferSize)
 	n, e := in.Read(buffer)
@@ -31,7 +39,7 @@ func Tally(in io.Reader, out io.Writer) error {
 	// iterate over all lines
 	for _, l := range strings.Split(entries, "\n") {
 		// there are some empty lines in the input
-		if l == "" {
+		if l == "" || string(l[0]) == "#" {
 			continue
 		}
 
@@ -43,25 +51,59 @@ func Tally(in io.Reader, out io.Writer) error {
 		// make sure both teams are already in our map
 		for i := 0; i < 2; i++ {
 			if _, b := m[w[0]]; !b { // if team does not yet exist
-				m[w[0]] = [2]int{0, 0} // create entry
+				m[w[0]] = entry{w[0], 0, 0, 0} // create entry
 			}
 		}
 
 		// add points to map
 		switch w[2] {
 		case "win":
-			m[w[0]] = [2]int{m[w[0]][0] + 1, m[w[0]][1]} // add one to wins for team 1
-			m[w[1]] = [2]int{m[w[1]][0], m[w[1]][1] + 1} // second team gets a loss
+			m[w[0]] = entry{w[0], m[w[0]].wins + 1, m[w[0]].losses, m[w[0]].draws} // add one to wins for team 1
+			m[w[1]] = entry{w[1], m[w[1]].wins, m[w[1]].losses + 1, m[w[1]].draws} // second team gets a loss
 		case "loss":
-			m[w[0]] = [2]int{m[w[0]][0], m[w[0]][1] + 1} // add one to loss for team 1
-			m[w[1]] = [2]int{m[w[1]][0] + 1, m[w[1]][1]} // second team gets a win
+			m[w[0]] = entry{w[0], m[w[0]].wins, m[w[0]].losses + 1, m[w[0]].draws} // add one to loss for team 1
+			m[w[1]] = entry{w[1], m[w[1]].wins + 1, m[w[1]].losses, m[w[1]].draws} // second team gets a win
 		case "draw": // do nothing
-			break
+			m[w[0]] = entry{w[0], m[w[0]].wins, m[w[0]].losses, m[w[0]].draws + 1} // add one to loss for team 1
+			m[w[1]] = entry{w[1], m[w[1]].wins, m[w[1]].losses, m[w[1]].draws + 1} // second team gets a win
 		default:
 			return errors.New("game result could not be parsed for " + w[0] + " : " + w[1])
 		}
 	}
 
 	// build output
+	//	separatorIndex := strings.Index(TableHeader, "|")
+	out.Write([]byte(TableHeader))
+	out.Write([]byte("\n"))
+
+	numberOfTeams := len(m)
+
+	for i := 0; i < numberOfTeams; i++ {
+		bestIndex := ""
+		// search highest scoring team
+		for k, v := range m {
+			// first iteration
+			if bestIndex == "" {
+				bestIndex = k
+				continue
+			}
+
+			if v.wins > m[bestIndex].wins {
+				bestIndex = k
+			} else if v.wins == m[bestIndex].wins {
+				if v.losses < m[bestIndex].losses {
+					bestIndex = k
+				}
+			}
+		}
+
+		bk := m[bestIndex]
+
+		// pads each team name to 25 characters, added after the name (-)
+		totalPoints := 3*bk.wins + bk.draws
+		out.Write([]byte(fmt.Sprintf("%-31v|  %v |  %v |  %v |  %v |  %v\n", bk.name, (bk.wins + bk.losses + bk.draws), bk.wins, bk.draws, bk.losses, totalPoints)))
+		delete(m, bestIndex)
+	}
+
 	return nil
 }
