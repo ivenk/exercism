0ec26a819f93cda149fdabd574b2f2d675c594b7
iven_k <koethke@th-brandenburg.de>
2019-08-13 21:09:22 +0200

    Updated solution

--- go/tournament/tournament.go
+++ go/tournament/tournament.go
@@ -1,25 +1,41 @@
 package tournament
 
 import (
+	"fmt"
+	"io"
 	"strconv"
 	"strings"
 )
 
+const bufferSize int = 1000
+
 // map structure: [name_of_the_team] = [wins, losses]
 
 // Tally converts the given input string into a table and returns it
-func Tally(results string) string {
+func Tally(reader io.Reader, buffer io.Writer) error {
 	m := make(map[string][2]int)
+	resultbytes := make([]byte, bufferSize) // dont know the size; but we want to read it all
 
-	for _, l := range strings.Split(results, "\n") { // split on new lines
-		w := strings.Split(l, ";")
-		if v, b := m[w[0]]; b {
-			m[w[0]] = [...]int{v[0] + strconv.Atoi(w[1]), v[1] + strconv.Atoi(w[2])}
-		} else {
-			// add entry with the described format
-			m[w[0]] = [...]int{w[1], w[2]}
+	for hasMore := true; hasMore; {
+		i, _ := reader.Read(resultbytes) // this is really redundant but i cant think of a bette way ...
+		if i > 0 {
+
 		}
 	}
 
-	return ""
+	fmt.Printf("%d\n", i)
+	results := string(resultbytes)
+
+	for _, l := range strings.Split(results, "\n") { // split on new lines
+		w := strings.Split(l, ";")
+		v1, _ := strconv.Atoi(w[1])
+		v2, _ := strconv.Atoi(w[2])
+		if v, b := m[w[0]]; b {
+			m[w[0]] = [...]int{v[0] + v1, v[1] + v2}
+		} else {
+			// add entry with the described format
+			m[w[0]] = [...]int{v1, v2}
+		}
+	}
+	return nil
 }
