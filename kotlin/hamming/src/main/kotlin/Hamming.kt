81eb262c9b1bd2f632898c6cb69bbc765d9e9d2c
ivenk <ivenkoethke@hotmail.de>
2020-09-07 19:26:04 +0200

    First solution to hamming

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -1,6 +1,11 @@
+import java.lang.IllegalArgumentException
+
 object Hamming {
 
     fun compute(leftStrand: String, rightStrand: String): Int {
-        TODO("Implement this function to complete the task")
+        if (leftStrand.length != rightStrand.length) throw IllegalArgumentException("left and right strands must be of equal length")
+        return leftStrand.zip(rightStrand).count {
+            it.first != it.second
+        }
     }
 }
