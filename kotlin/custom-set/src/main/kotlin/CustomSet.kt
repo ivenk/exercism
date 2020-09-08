9ed3209604ebeef7ccd5977b812aac8e890ee958
ivenk <ivenkoethke@hotmail.de>
2020-09-08 20:49:40 +0200

    Work in progress on exercise custom set

---
+++ kotlin/custom-set/src/main/kotlin/CustomSet.kt
@@ -0,0 +1,45 @@
+class CustomSet(vararg input: Int) {
+
+    var content: MutableList<Int> = input.toMutableList()
+
+    fun isEmpty(): Boolean {
+        return content.isEmpty()
+    }
+
+    fun isSubset(other: CustomSet): Boolean {
+        return content.containsAll(other.content)
+    }
+
+    fun isDisjoint(other: CustomSet): Boolean {
+        return !content.any { other.contains(it) }
+    }
+
+    fun contains(other: Int): Boolean {
+        return content.contains(other)
+    }
+
+    fun intersection(other: CustomSet): CustomSet {
+        throw NotImplementedError();
+    }
+
+    fun add(other: Int) {
+        content.add(other)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return if (other is CustomSet) content == content
+        else false
+    }
+
+    operator fun plus(other: CustomSet): CustomSet {
+        val newList = content.toMutableList()
+        newList.addAll(other.content)
+        return CustomSet().apply { this.content = newList }
+    }
+
+    operator fun minus(other: CustomSet): CustomSet {
+        val newList = content.toMutableList()
+        newList.removeAll(other.content)
+        return CustomSet().apply { this.content = newList }
+    }
+}
